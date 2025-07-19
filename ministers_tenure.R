6d626452cabbc509c62986197d97117a2cd77ed2
Jason18 <164468@unsaac.edu.pe>
2025-07-18 22:28:04 -0500

    Fixed warning messages

--- ministers_tenure.R
+++ ministers_tenure.R
@@ -12,14 +12,17 @@ df <- read_excel("C:/Users/Jason Cruz/OneDrive/Escritorio/mef_ministers_dataset.
 
 # Compute duration
 df <- df %>%
-  mutate(duration_days = as.numeric(difftime(end_date, start_date, units = "days")))
+  mutate(
+    duration_days = as.numeric(difftime(end_date, start_date, units = "days")),
+    minister_name = fct_reorder(minister_name, start_date)
+  )
 
 # Create the plot
 ministers_tenure_plot <- ggplot(df, aes(x = start_date, xend = end_date, y = minister_name, yend = minister_name)) +
   geom_segment(aes(color = duration_days), linewidth = 5) +
   scale_color_gradientn(
     colours = c("#005f5e", "#00DFA2", "#FFF183", "#ff8575", "#E6004C"),
-    values = scales::rescale(c(1000, 700, 400, 200, 0)),
+    values = rescale(c(1000, 700, 400, 200, 0)),
     name = "Days in Office"
   ) +
   scale_x_datetime(date_labels = "%Y", date_breaks = "2 year") +
@@ -28,25 +31,27 @@ ministers_tenure_plot <- ggplot(df, aes(x = start_date, xend = end_date, y = min
     x = "",
     y = "Minister"
   ) +
-  theme_minimal(base_size = 13) +
+  theme_minimal(base_size = 14) +
   theme(
-    # Axis text styling
-    axis.text.y = element_text(size = 12),
-    axis.text.x = element_text(size = 12),  # Increased font size for X-axis
+    axis.text.y = element_text(size = 14),
+    axis.text.x = element_text(size = 14),
     plot.title = element_text(face = "bold"),
-    
-    # Grid lines
-    panel.grid.major = element_line(color = "#f5f5f5", size = 0.65),
-    panel.grid.minor = element_line(color = "#f5f5f5", size = 0.65),
-    
-    # Panel border
-    panel.border = element_rect(color = "black", fill = NA, size = 0.5),
-    
-    # Legend positioning and style
-    legend.position = c(0.98, 0.02),  # Bottom right inside chart
+    panel.grid.major = element_line(color = "#f5f5f5", linewidth = 0.65),
+    panel.grid.minor = element_line(color = "#f5f5f5", linewidth = 0.65),
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5),
+    legend.position = c(0.98, 0.02),
     legend.justification = c("right", "bottom"),
-    legend.background = element_rect(fill = scales::alpha("#f5f5f5", 0.45), color = "#f5f5f5", size = 1)
+    legend.background = element_rect(
+      fill = alpha("#f5f5f5", 0.5),  # 50% transparency
+      color = "#f5f5f5",
+      linewidth = 0.75
+    )
   )
 
-# Guardar como PNG
-ggsave("C:/Users/Jason Cruz/OneDrive/Documentos/ministers_tenure.png", plot = ministers_tenure_plot, width = 12, height = 6, dpi = 300, bg = "white"")
+# Save as PNG with white background
+ggsave("C:/Users/Jason Cruz/OneDrive/Escritorio/ministers_tenure.png",
+       plot = ministers_tenure_plot,
+       width = 12,
+       height = 6,
+       dpi = 300,
+       bg = "white")
